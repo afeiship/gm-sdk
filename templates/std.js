4cc76c96b1d210b9e210eb15f9d794acc03f29d3
afeiship <1290657123@qq.com>
2020-07-07 18:57:50 +0800

    feat: initial

---
+++ templates/std.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         <%= scope %>
+// @namespace    <%= namespace %>
+// @version      1.0.0
+// @description  <%= description %>
+// @author       <%= author %>
+// @match        *://*/*
+// @require  https://cdn.jsdelivr.net/npm/@feizheng/next-js-core2@2.5.0/dist/next-js-core2.js
+// @require tampermonkey://vendor/jquery.js
+// @grant GM_addStyle
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_addValueChangeListener
+// @grant GM_removeValueChangeListener
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_log
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_registerMenuCommand
+// @grant GM_unregisterMenuCommand
+// @grant GM_openInTab
+// @grant GM_xmlhttpRequest
+// @grant GM_download
+// @grant GM_getTab
+// @grant GM_saveTab
+// @grant GM_getTabs
+// @grant GM_notification
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant  window.focus
+// @grant  window.close
+// @icon64 https://tva1.sinaimg.cn/large/007S8ZIlgy1gexw87htqhj305k05k74o.jpg
+// ==/UserScript==
