d13ff4b0e60d821ed3404f349ae130568e7ec3a3
afeiship <1290657123@qq.com>
2020-10-13 14:02:29 +0800

    fix: conflict jquery

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
-import nx from '@feizheng/next-js-core2';
-import NxGmXhr from '@feizheng/next-gm-xhr';
-import NxGmStorage from '@feizheng/next-gm-storage';
 import NxGmApi from '@feizheng/next-gm-api';
+import NxGmStorage from '@feizheng/next-gm-storage';
+import NxGmXhr from '@feizheng/next-gm-xhr';
+import nx from '@feizheng/next-js-core2';
 
 var http = NxGmXhr.getInstance();
 var apis = NxGmApi.generate(nx.GLOBAL);
 var store = new NxGmStorage('aric');
 var gmVersion = '__VERSION__';
 
-
 nx.declare({
   statics: {
     init: function () {
-      nx.mix(
-        unsafeWindow,
-        {
-          nx: unsafeWindow.nx || nx,
-          $: nx.mix($, {
-            version: $.fn.jquery,
-          }),
-          gmsdk: this.sdk()
-        }
-      );
+      nx.mix(unsafeWindow, {
+        nx: unsafeWindow.nx || nx,
+        $: nx.mix(unsafeWindow.$, {
+          version: $.fn.jquery
+        }),
+        gmsdk: this.sdk()
+      });
     },
     sdk: function () {
-      return nx.mix({
-        version: gmVersion,
-        http,
-        store,
-      }, apis);
+      return nx.mix(
+        {
+          version: gmVersion,
+          http,
+          store
+        },
+        apis
+      );
     }
   }
 });
